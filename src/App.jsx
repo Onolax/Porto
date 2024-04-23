e2129e6c26d2094e2bbf4219e27af7578fa2fa05
Onolax <meenamonu2002@gmail.com>
2024-04-24 05:07:29 +0530

    initialize

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  return (
+    <div className='bg-green-400'>
+      <div className=' '>
+        <header>
+          <h1>Monu</h1>
+        </header>
+      </div>
+    </div>
+  )
+}
+
+export default App
